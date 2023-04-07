f4aeaf7cd53ea3266548ce9bee4217fd8b0b3ecf
rianpediaa <126028334+rianpediaa@users.noreply.github.com>
2023-04-07 11:40:13 +0700

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,5 +1,5 @@
 module.exports = {
-  url: "mongodb://localhost:27017/",
+  url: "mongodb+srv://sohardtod:HsohardtodH@cluster0.euiqg3v.mongodb.net/?retryWrites=true&w=majority",
   database: "bezkoder_files_db",
   imgBucket: "photos",
 };
